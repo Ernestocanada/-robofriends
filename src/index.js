ddef97a4ab24deb7eeb8528bd682d6f76a1b2811
Ernestocanada <ernesto.industrial@gmail.com>
2021-09-18 17:47:16 +0200

    make it better7

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
